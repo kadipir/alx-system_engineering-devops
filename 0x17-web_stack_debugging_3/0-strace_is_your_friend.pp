e96d04c38839fcc0452edb8d5e9ca015685673bc
truth <trutherkadi@gmail.com>
2024-06-20 22:37:17 -0700

    puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-#fixes bad "phpp" extensions to "php" in "wp-sttings.php"
+# Puppet manifest to fix a bug in wp-setings.php
 
-exec{'fix-wordpress':
-     command => 'sed i s/phpp/php/g /var/www/html/wp-settings.php',
-     path    => 'usr/local/bin/:bin'
-
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
